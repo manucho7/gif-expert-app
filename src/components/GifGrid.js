4c5d1f3b8c82372d41a529f9783e3f8d7f672bfe
Manuel Blanco <manuel.blanco77@gmail.com>
2020-08-17 14:27:33 -0300

    Subiendo ajustes al css, <GifGrid/>, <GifGridItem/>

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
+import GifGridItem from "./GifGridItem";
 
 export const GifGrid = ({ category }) => {
 
-    const [count, setCount] = useState(0);
+    const [images, setImages] = useState([]);
 
     useEffect( () => {
         getGifs();
@@ -24,18 +25,29 @@ export const GifGrid = ({ category }) => {
     });
 
     console.log(gifs);
+    setImages( gifs );
     };
 
 //    getGifs();
 
     return (
         <>
-            <h3> {category} </h3>
-            <h3> {count} </h3>
+            <h3> { category } </h3>
+            <div className="card-grid">
 
-            <button onClick={() => setCount(count + 1)}></button>
+
+                {
+                    images.map( img => (
+                        <GifGridItem
+                            key={ img.id }
+                            { ...img }
+                    />
+                    ))
+                }
+
+            </div>
         </>
-    );
+    )
 };
 
 export default GifGrid;
