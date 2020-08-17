0c9367b921868af30ed45790ebd79a50bd2a7001
Manuel Blanco <manuel.blanco77@gmail.com>
2020-08-17 15:19:28 -0300

    Subiendo cambios en <AddCategory/>, <GifGrid/> y creando herlpers

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,34 +1,15 @@
 import React, { useState, useEffect } from "react";
 import GifGridItem from "./GifGridItem";
+import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
 
     const [images, setImages] = useState([]);
 
     useEffect( () => {
-        getGifs();
-    }, [])
-
-    const getGifs = async () => {
-
-    const url =
-        "https://api.giphy.com/v1/gifs/search?q=rick and morty&limit=10&api_key=EKYfdfTGfUEzUTT5g5wl1ONxKqC6u2Rx";
-    const resp = await fetch(url);
-    const { data } = await resp.json();
-
-    const gifs = data.map((img) => {
-        return {
-            id: img.id,
-            title: img.title,
-            url: img.images?.downsized_medium.url,
-        };
-    });
-
-    console.log(gifs);
-    setImages( gifs );
-    };
-
-//    getGifs();
+        getGifs( category )
+            .then( setImages )
+    }, [ category ])
 
     return (
         <>
