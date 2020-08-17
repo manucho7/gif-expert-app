8e09b4c717d31a435881ba1483b1e9ee9fe26409
Manuel Blanco <manuel.blanco77@gmail.com>
2020-08-17 13:36:24 -0300

    GifGrid useState, useEffect

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,31 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 export const GifGrid = ({ category }) => {
 
+    const [count, setCount] = useState(0);
+
+    useEffect( () => {
+        getGifs();
+    }, [])
+
     const getGifs = async () => {
-        const url = 'https://api.giphy.com/v1/gifs/search?q=rick and morty&limit=10&api_key=EKYfdfTGfUEzUTT5g5wl1ONxKqC6u2Rx'
-        const respuesta = await fetch(url);
-        const { data } = await respuesta.json();
 
-        const gifs = data.map( img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        })
+    const url =
+        "https://api.giphy.com/v1/gifs/search?q=rick and morty&limit=10&api_key=EKYfdfTGfUEzUTT5g5wl1ONxKqC6u2Rx";
+    const resp = await fetch(url);
+    const { data } = await resp.json();
 
-        console.log(gifs);
+    const gifs = data.map((img) => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url,
+        };
+    });
 
-    }
+    console.log(gifs);
+    };
 
-    getGifs();
+//    getGifs();
 
     return (
-            <>
-                <h3>{ category }</h3>
-            </>
-    )
-}
+        <>
+            <h3> {category} </h3>
+            <h3> {count} </h3>
+
+            <button onClick={() => setCount(count + 1)}></button>
+        </>
+    );
+};
 
 export default GifGrid;
