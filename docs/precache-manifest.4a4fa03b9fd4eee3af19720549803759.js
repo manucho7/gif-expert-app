68ffddb4de9d80578377b1b8d7ce8bff1ca1d7e2
manucho7 <manuel.blanco77@gmail.com>
2020-08-18 09:51:15 -0300

    Subiendo docs

---
+++ docs/precache-manifest.4a4fa03b9fd4eee3af19720549803759.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a74dc5b181dcc1124f97d0e82b756c89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27fc7cfc5ecce61aea45",
+    "url": "/static/css/main.3c2b9b6c.chunk.css"
+  },
+  {
+    "revision": "9948515d998a14a1480d",
+    "url": "/static/js/2.6a91807f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a91807f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27fc7cfc5ecce61aea45",
+    "url": "/static/js/main.7ff5c197.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);
