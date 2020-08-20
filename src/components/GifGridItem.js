5ad89ad12c8a57eac6be23be1aadaf29e8581a10
manucho7 <manuel.blanco77@gmail.com>
2020-08-20 09:54:30 -0300

    Subiendo cambios en componente <GifGridItem/> y configurando pruebas

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
 export const GifGridItem = ({ title, url }) => {
+  return (
+    <div className="card animate__animated animate__fadeIn">
+        <img src={url} alt={title} />
+        <p>{title}</p>
+    </div>
+  );
+};
 
-    return (
-        <div className='card animate__animated animate__fadeIn'>
-            <img src={ url } alt={ title } />
-            <p>{ title }</p>
-        </div>
-    )
-}
+GifGridItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+};
 
 export default GifGridItem;
--- src/tests/components/GifGridItems.test.js
+++ src/tests/components/GifGridItems.test.js
@@ -6,14 +6,15 @@ import '@testing-library/jest-dom';
 
 
 describe('Pruebas en <GifGridItem/>', () => {
+
+    const title = 'Un titulo';
+    const url = 'https://localhost/algo.jpg';
     
     test('debe de mostrar el componente correctamente', () => {
 
-        const wrapper = shallow( <GifGridItem /> )
-
+        const wrapper = shallow( <GifGridItem title={ title } url={ url }/> )
         expect( wrapper ).toMatchSnapshot();
 
     })
-
 
 })
