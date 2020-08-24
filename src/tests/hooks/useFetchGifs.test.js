3d9b4c7a5eb35b7323699e2b8a867ac831f5d0e4
manucho7 <manuel.blanco77@gmail.com>
2020-08-24 13:04:33 -0300

    Subiendo pruebas en Hooks pt2

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -5,13 +5,28 @@ import { renderHook } from '@testing-library/react-hooks';
 
 describe('Pruebas en el hook useFetchGifs', () => {
     
-    test('debe de retornar el estado inicial ', () => {
+    test('debe de retornar el estado inicial ', async () => {
 
-        const { result } = renderHook( () => useFetchGifs( '420' ) );
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs( '420' ) );
         const { data, loading } = result.current;
+
+        await waitForNextUpdate(); 
 
         expect( data ).toEqual([]);
         expect( loading ).toBe( true );
+
+    });
+    
+    test('debe de retornar un array de imgs y loading en false', async () => {
+        
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs( 'One Punch' ) );
+        
+        await waitForNextUpdate(); 
+
+        const { data, loading } = result.current;
+
+        expect( data.length ).toBe( 10 );
+        expect( loading ).toBe( false );
 
     });
     
